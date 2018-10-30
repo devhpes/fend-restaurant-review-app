35f5928b9058a51c08ad5b3c7af3dc9720bf9165
Abhishek Singh <abhi.devwork@outlook.com>
2018-10-31 01:12:02 +0530

    url fix dbhelper

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     //const port = 5500 // Change this to your server port
-    return `//github.com/devhpes/fend-restaurant-review-app/blob/master/data/restaurants.json`;
+    return `https://devhpes.github.io/fend-restaurant-review-app/blob/master/data/restaurants.json`;
   }
 
   /**
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return `//github.com/devhpes/fend-restaurant-review-app/tree/master/img/${restaurant.photograph}`;
+    return `https://devhpes.github.io/fend-restaurant-review-app/tree/master/img/${restaurant.photograph}`;
   }
 
   /**
