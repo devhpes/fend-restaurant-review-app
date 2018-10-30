7a98620f8016932239938c58cc8353599257865f
Abhishek Singh <abhi.devwork@outlook.com>
2018-10-31 01:09:56 +0530

    url fix

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     //const port = 5500 // Change this to your server port
-    return `https://github.com/devhpes/fend-restaurant-review-app/blob/master/data/restaurants.json`;
+    return `//github.com/devhpes/fend-restaurant-review-app/blob/master/data/restaurants.json`;
   }
 
   /**
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`https://github.com/devhpes/fend-restaurant-review-app/tree/master/img/${restaurant.photograph}`);
+    return `//github.com/devhpes/fend-restaurant-review-app/tree/master/img/${restaurant.photograph}`;
   }
 
   /**
