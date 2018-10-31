043b5814556916434668e6a195de22de191afb1a
Abhishek Singh <abhi.devwork@outlook.com>
2018-10-31 22:13:49 +0530

    service worker fetch

--- sw.js
+++ sw.js
@@ -48,36 +48,37 @@ self.addEventListener('activate', function(event) {
 
 //Adding fetch and responding caches
 self.addEventListener('fetch', function(event) {
-// Handling request and checking the cache
   event.respondWith(
     caches.match(event.request)
-    .then(function(response) {
-    // If there was an error return the response
-      if (response) {
-        return response;
-      }
-
-      var cacheFetchRequest = event.request.clone();
-
-      return fetch(cacheFetchRequest).then(
-        function(response) {
-          // Check if we received a valid response
-          if(!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          //Cloning the reponse again
-          var responseToCache = response.clone();
-
-          caches.open(staticCacheName)
-            .then(function(cache) {
-              cache.put(event.request, responseToCache);
-            });
-
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
           return response;
         }
-      )
-    })
-  );
+
+        //Cloning the request
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            //Cloning the response
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
 
 // Skip waiting service worker and become an active one
